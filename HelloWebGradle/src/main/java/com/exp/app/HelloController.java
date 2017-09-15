ca6af9bfa034bd3e8bd1a48eae629a5891b83d67
muhammad suta <muhammad.suta75@gmail.com>
2017-09-15 16:58:34 +0700

    commit first

---
+++ HelloWebGradle/src/main/java/com/exp/app/HelloController.java
@@ -0,0 +1,13 @@
+package com.exp.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "hello world";
+	}
+}
---
+++ HelloWebGradle/src/main/java/com/exp/app/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.exp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.exp"})
+public class HelloWorldApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+	
+}
